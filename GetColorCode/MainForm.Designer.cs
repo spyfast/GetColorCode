1c9181e5c2c135cc3fb659388e6825e6ef6a35ff
Владимир <53353198+spyfast@users.noreply.github.com>
2020-10-28 01:32:16 +0300

    Add files via upload

---
+++ GetColorCode/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GetColorCode
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimeUpdates = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_CodeColor = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TimeUpdates
+            // 
+            this.TimeUpdates.Enabled = true;
+            this.TimeUpdates.Interval = 1;
+            this.TimeUpdates.Tick += new System.EventHandler(this.TimeUpdates_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label_CodeColor);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(576, 44);
+            this.panel1.TabIndex = 0;
+            // 
+            // label_CodeColor
+            // 
+            this.label_CodeColor.AutoSize = true;
+            this.label_CodeColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_CodeColor.Location = new System.Drawing.Point(227, 12);
+            this.label_CodeColor.Name = "label_CodeColor";
+            this.label_CodeColor.Size = new System.Drawing.Size(93, 20);
+            this.label_CodeColor.TabIndex = 0;
+            this.label_CodeColor.Text = "Code color: ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 352);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GetColorCode";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimeUpdates;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_CodeColor;
+    }
+}
+
---
+++ GetColorCode/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace GetColorCode
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point point);
+
+        [DllImport("gdi32.dll", 
+            CharSet = CharSet.Auto,
+            SetLastError = true,
+            ExactSpelling = true)]
+        static extern int BitBlt(
+            IntPtr intPrt,
+            int x,
+            int y,
+            int width,
+            int height,
+            IntPtr intPrt2,
+            int xSrc,
+            int ySrc,
+            int dwRop);
+
+        Bitmap screen = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public Color GetPixelColor(Point point)
+        {
+            using (Graphics graphics = Graphics.FromImage(screen))
+            {
+                using (Graphics graphics1 = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    var src = graphics1.GetHdc();
+                    var src2 = graphics.GetHdc();
+                    var val = BitBlt(src2, 0, 0, 1, 1, src, point.X, point.Y,
+                        (int)CopyPixelOperation.SourceCopy);
+                    graphics1.ReleaseHdc();
+                }
+            }
+
+            return screen.GetPixel(0, 0);
+        }
+
+        private void TimeUpdates_Tick(object sender, EventArgs e)
+        {
+            var point = new Point();
+            GetCursorPos(ref point);
+
+            var cursor = GetPixelColor(point);
+            BackColor = cursor;
+            label_CodeColor.Text = "Code color: " + BackColor.Name;
+        }
+    }
+}
